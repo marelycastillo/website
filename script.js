f2afea6ed616ab7f802ae83b2b11c66a09d5c471
marelycastillo <138072493+marelycastillo@users.noreply.github.com>
2023-06-28 17:28:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+var button = document.getElementById("hi");
+var body = document.body;
+button.addEventListener("click", function(){
+
+
+    console.log("hi");
+    body.setAttribute("style", "background-color:black ");
+
+})
